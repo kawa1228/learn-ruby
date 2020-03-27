f039f83b02ac4a02ae50f475bfaa673a87d7c103
kawa1228 <sxa.1105bo@gmail.com>
2020-03-27 23:21:18 +0900

    ブロックについてコメントを追加

--- src/for.rb
+++ src/for.rb
@@ -5,7 +5,12 @@ for n in numbers
 end
 puts sum #10
 
+
 # rubyでは通常forよりeachを使う
+
+# ブロック（do ... end）
+# メソッドの引数処理のかたまりを渡す
+# rubyの言語機能ではなく配列のeachメソッドが繰り返し処理される
 numbers = [1, 2, 3, 4]
 sum_2 = 0
 numbers.each do |n|
